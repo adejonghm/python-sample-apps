47a8b5ff2dacbee462c43a494ca21b73d4355fb0
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-02-09 09:44:16 -0300

    upd: Added Show function in GUI

--- 3.ToDo/gui.py
+++ 3.ToDo/gui.py
@@ -15,11 +15,13 @@ import tdlib as fn
 
 
 label_input = sg.Text('Type a To-Do')
+label_list = sg.Text('My To-Do list')
 input_text = sg.InputText(key='item_to_add', tooltip='Enter the text')
 btn_add = sg.Button('Add', key='Add')
-
+listbox = sg.Listbox(values=fn.get_toDos(), key='list_item',
+                     enable_events=True, size=[51, 12])
                      
-layout = [[label_input], [input_text, btn_add]]
+layout = [[label_input], [input_text, btn_add], [label_list], [listbox],]
 window = sg.Window('To-Do List App', layout=layout, font=('Helvetica', 11))
 
 while True:
