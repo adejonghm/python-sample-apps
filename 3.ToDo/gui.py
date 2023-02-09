a41bfc7d57370883bd8a71ed9ccb3b62eb3ed7d2
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-02-09 09:41:35 -0300

    upd: Added Graphic User Interface for to-do app

--- 3.ToDo/gui.py
+++ 3.ToDo/gui.py
@@ -11,26 +11,30 @@ Jan 29, 2023
 
 # Third party imports
 import PySimpleGUI as sg
-import tdlib as td
+import tdlib as fn
 
 
-label = sg.Text("Type in a To-Do")
-input_box = sg.InputText(key='input_box', tooltip='Enter the text')
-add_button = sg.Button("Add")
+label_input = sg.Text('Type a To-Do')
+input_text = sg.InputText(key='item_to_add', tooltip='Enter the text')
+btn_add = sg.Button('Add', key='Add')
 
-windows = sg.Window('To-Do List App',
-                    layout=[[label], [input_box, add_button]],
-                    font=('Helvetica', 11))
+                     
+layout = [[label_input], [input_text, btn_add]]
+window = sg.Window('To-Do List App', layout=layout, font=('Helvetica', 11))
 
 while True:
-    event, values = windows.read()
+    event, values = window.read()
+
     match event:
         case "Add":
-            toDos = td.read_toDos()
-            toDos.append(values['input_box'].capitalize() + '\n')
-            td.save_toDos(toDos)
-        
+            toDos = fn.get_toDos()
+            toDos.append(values['item_to_add'].capitalize() + '\n')
+            fn.save_toDos(toDos)
+            
+            window['list_item'].update(values=toDos)
+            window['item_to_add'].update(value='')
+
         case sg.WIN_CLOSED:
             break
 
-windows.close()
+window.close()
