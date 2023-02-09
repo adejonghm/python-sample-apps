eef7846164c6ea31a6d6380e3fe317ea622264ad
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-02-09 15:36:43 -0300

    v1: First Version with GUI

---
+++ 3.ToDo/ToDo-CLI.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+"""
+Developed by adejonghm
+----------
+
+Nov 10, 2022
+"""
+
+# Standard library imports
+import time
+
+# Third party imports
+import tdlib as td
+
+
+if __name__ == "__main__":
+    now = time.strftime("%b %d, %Y %H:%S")
+    print("It is", now)
+
+    while True:
+        print(
+            "Use one of the following commands [add, show, edit, complete or exit]")
+        user_action = input("Enter the command: ")
+        user_action = user_action.strip()
+
+        toDos = td.get_toDos()
+
+        if user_action.startswith('add'):
+            item = user_action[4:]
+            if item:
+                toDos.append(item.capitalize() + '\n')
+                td.save_toDos(toDos)
+                print('*** New item has been added successfully! ***\n')
+            else:
+                print("*** Type the ToDo item you want to ADD after the command. ***\n")
+
+        elif user_action.startswith('show'):
+            # new_toDos = [item.strip("\n") for item in toDos]
+            for index, item in enumerate(toDos):
+                item = item.strip('\n')
+                line = f"{index + 1}. {item}"
+                print(line)
+
+        elif user_action.startswith('edit'):
+            try:
+                number = int(user_action[5:])
+                new_item = input("Enter the new ToDo item: ")
+
+                toDos[number - 1] = new_item.capitalize() + "\n"
+
+                td.save_toDos(toDos)
+                print(
+                    f"*** Item {number} has been updated successfully! ***\n")
+
+            except ValueError:
+                print(
+                    "*** Type the item number you want to EDIT after the command. ***\n")
+                continue
+
+        elif user_action.startswith('complete'):
+            try:
+                number = int(user_action[9:])
+
+                index = number - 1
+
+                toDo_to_remove = toDos[index].strip('\n')
+                toDos.pop(index)
+
+                td.save_toDos(toDos)
+
+                print(
+                    f"*** Item '{toDo_to_remove}' has been completed successfully! ***\n")
+
+            except IndexError:
+                print("*** There is no ITEM with that number. ***\n")
+                continue
+
+            except ValueError:
+                print(
+                    "*** Type the item number you want to COMPLETE after the command. ***\n")
+                continue
+
+        elif user_action.startswith(('exit', 'quit')):
+            break
+
+        else:
+            print("*** Please enter a valid command. ***\n")
+
+    print("Bye!")
---
+++ 3.ToDo/ToDo-UI.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+"""
+Developed by adejonghm
+----------
+
+Jan 29, 2023
+"""
+
+# Standard library imports
+
+# Third party imports
+import PySimpleGUI as sg
+import tdlib as fn
+
+
+label_input = sg.Text('Type a To-Do')
+label_list = sg.Text('My To-Do List')
+input_text = sg.InputText(key='item_to_add', tooltip='Enter the text')
+btn_add = sg.Button('Add', key='Add')
+btn_edit = sg.Button('Edit', key='Edit')
+btn_complete = sg.Button('Done', key='Complete')
+listbox = sg.Listbox(values=fn.get_toDos(), key='list_item',
+                     enable_events=True, size=[51, 12])
+                     
+layout = [[label_input], [input_text, btn_add], [label_list], [listbox], [btn_complete, btn_edit]]
+window = sg.Window('To-Do List App', layout=layout, font=('Helvetica', 11))
+
+while True:
+    event, values = window.read()
+
+    match event:
+        case "Add":
+            if values['item_to_add'] != '':
+                toDos = fn.get_toDos()
+                toDos.append(values['item_to_add'].capitalize() + '\n')
+                fn.save_toDos(toDos)
+            
+                window['list_item'].update(values=toDos)
+                window['item_to_add'].update(value='')
+
+            else:
+                sg.popup('Please, Enter the To-Do you want to add.', title='Warning!', keep_on_top=True)
+                
+        case "Edit":
+            try:
+                item_to_edit = values['list_item'][0]
+                new_item = values['item_to_add']
+
+                toDos = fn.get_toDos()
+                index = toDos.index(item_to_edit)
+                toDos[index] = new_item.capitalize() + '\n'
+                fn.save_toDos(toDos)
+
+                window['list_item'].update(values=toDos)
+                window['item_to_add'].update(value='')
+
+            except IndexError:
+                sg.popup('Please, select an item first.', title='Warning!', keep_on_top=True)
+
+        case "Complete":
+            try:
+                item_to_delete = values['list_item'][0]
+
+                toDos = fn.get_toDos()
+                toDos.remove(item_to_delete)
+                fn.save_toDos(toDos)
+
+                window['list_item'].update(values=toDos)
+                window['item_to_add'].update(value='')
+
+            except IndexError:
+                sg.popup('Please, select an item first.', title='Warning!', keep_on_top=True)
+
+        case "list_item":
+            item_selected = values['list_item'][0].strip('\n')
+
+            window['item_to_add'].update(value=item_selected)
+
+        case sg.WIN_CLOSED:
+            break
+
+window.close()
