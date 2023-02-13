0fcc813a2e23bb4833a6d3e46d5e2822645a076d
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-02-13 10:06:01 -0300

    feat: Add-New-Item feature was added.

--- 3.ToDo/ToDo-Web.py
+++ 3.ToDo/ToDo-Web.py
@@ -17,9 +17,19 @@ import tdlib as fn
 
 toDos = fn.get_toDos()
 
+
+def add_new_item():
+    todo = st.session_state['new_item'] + '\n'
+    toDos.append(todo)
+
+    fn.save_toDos(toDos)
+
+
 st.title('To-Do Web App')
 
 st.write("My To-Do list:")
 for td in toDos:
     st.checkbox(td)
 
+st.text_input("Enter the To-Do:", on_change=add_new_item,
+              placeholder="Type here...", key='new_item')
