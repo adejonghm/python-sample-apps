0ada9dab0ca17e0235ff192f9f10325e94b99f1b
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-02-24 18:58:00 -0300

    fix: Improved in the web interface

--- 3.ToDo/ToDo-Web.py
+++ 3.ToDo/ToDo-Web.py
@@ -20,14 +20,19 @@ toDos = fn.get_toDos()
 
 
 def add_new_item():
+    """ Function """
     todo = st.session_state['new_item'] + '\n'
     toDos.append(todo)
     fn.save_toDos(toDos)
 
-
+# st.set_page_config(layout="wide")
 st.title('To-Do Web App')
 
-st.write("My To-Do list:")
+st.text_input(label="input-todo", key='new_item',
+              placeholder="Type here...", label_visibility="hidden",
+              on_change=add_new_item)
+
+st.write("<h5><u>My To-Do list:</u></h5>", unsafe_allow_html=True)
 for index, td in enumerate(toDos):
     checked = st.checkbox(td, key=td)
     if checked:
@@ -35,6 +40,3 @@ for index, td in enumerate(toDos):
         fn.save_toDos(toDos)
         del st.session_state[td]
         st.experimental_rerun()
-
-st.text_input("Enter the To-Do:", key='new_item',
-              on_change=add_new_item, placeholder="Type here...")
