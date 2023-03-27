e1b1b8c9a005ac1ddca501a850cd5f4affccf56d
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-03-27 16:58:38 -0300

    feat: Added contact page with form

---
+++ 5.Portafolio/pages/Contact.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Developed by adejonghm
+----------
+
+March 14, 2023
+"""
+
+# Third-party libraries imports
+import pandas as pd
+import streamlit as st
+
+# Local libraries imports
+import email_lib as elib
+
+
+data = pd.read_csv("source/topics.csv")
+st.header("Contact Me")
+
+with st.form(key="contact_form"):
+    user_name = st.text_input("Your name")
+    user_email = st.text_input("Your email address")
+    topic = st.selectbox('What topic do you want to discuss?', data['topic'])
+    raw_message = st.text_area("Your message")
+    message = f"""\
+Subject: {user_name} has a {topic}.
+
+{raw_message}
+email: {user_email}
+"""
+    button = st.form_submit_button("Submit")
+
+    if button:
+        elib.send_email(message)
+        st.info("Your message sent successfully!")
